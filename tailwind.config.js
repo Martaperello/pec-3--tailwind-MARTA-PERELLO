7c89c49e3ccb245df01b3186882642932aef4e36
Marta Perelló <100278106+Martaperello@users.noreply.github.com>
2023-06-01 14:53:26 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      'bebas': ['Bebas Neue', 'sans-serif'],
+    },
+  },
+  variants: {
+  },
+  plugins: [
+  ]
+}
